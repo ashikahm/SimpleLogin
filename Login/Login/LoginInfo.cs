975e251e93f113b89d6f50fd5ad2800483643f2d
Ashika <ashikahm@gmail.com>
2022-09-26 14:12:30 +0530

    Add project files.

---
+++ Login/Login/LoginInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login
+{
+    public class LoginInfo
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var info = (LoginInfo)BindingContext;
+            message.Text = "Hai New User! " + "Your name is " + info.name;
+
+        }
+    }
+}
